be84d9dcfd3febfd06d115f43dd504f050874549
rygallagher <rgallagher@pssolutions.net>
2022-03-30 10:37:16 -0400

    home button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pssolutions-fun';
+    title = 'pssolutions-fun';
 
-  drawerOpen = false;
+    drawerOpen = false;
+
+    constructor(
+        private router: Router,
+    ) {}
+
+    goHome(): void {
+        this.router.navigate(['']);
+    }
 }

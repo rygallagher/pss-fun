880bdbb9ed5bcaac89bceae11d0309c235330b44
rygallagher <rgallagher@pssolutions.net>
2022-04-11 13:45:57 -0400

    4/11/2022

--- src/app/components/days-since-last-seen/days-since-last-seen.component.ts
+++ src/app/components/days-since-last-seen/days-since-last-seen.component.ts
@@ -12,24 +12,24 @@ interface Row {
 })
 export class DaysSinceLastSeenComponent  {
 	rows: Row[] = [
-		{name: 'Adam', days: '0'},
+		{name: 'Adam', days: '1'},
 		{name: 'Allen', days: '0'},
 		{name: 'Brandon', days: '0'},
-		{name: 'Chris', days: '1'},
-		{name: 'Denny', days: '1'},
+		{name: 'Chris', days: '2'},
+		{name: 'Denny', days: '0'},
 		{name: 'Dylan', days: '0'},
 		{name: 'Greg', days: '0'},
 		{name: 'Joe', days: '0'},
 		{name: 'Jon', days: '0'},
 		{name: 'Josh', days: '0'},
-		{name: 'Korry', days: '1'},
-		{name: 'Kayle', days: '3'},
+		{name: 'Korry', days: '0'},
+		{name: 'Kayle', days: '4'},
 		{name: 'Matthew', days: '0'},
 		{name: 'Mike', days: '0'},
-		{name: 'Paul', days: '20'},
+		{name: 'Paul', days: '21'},
 		{name: 'Ry', days: '0'},
 		{name: 'Timmy', days: '0'},
-		{name: 'Tyler', days: '15'},
-		{name: 'Wayne', days: '24'},
+		{name: 'Tyler', days: '16'},
+		{name: 'Wayne', days: '25'},
 	]
 }
